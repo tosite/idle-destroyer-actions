a4d604adb5d7b23dc98bc13294f9b8a9564ca8b6
tosite <teshima.naoto@moneyforward.co.jp>
2023-11-29 12:03:52 +0900

    wip

--- actions/initializer.rb
+++ actions/initializer.rb
@@ -4,18 +4,18 @@ require 'time'
 require '/actions/github_api_client'
 require '/actions/slack_client'
 
-keys = %w(GITHUB_BASE_URL GITHUB_TOKEN SLACK_WEBHOOK LIMIT_DAYS NOTIFY_TEMPLATE CLOSED_TEMPLATE)
+keys = %w(GITHUB_BASE_URL REPOSITORY GITHUB_TOKEN SLACK_WEBHOOK LIMIT_DAYS NOTIFY_TEMPLATE CLOSED_TEMPLATE)
 puts '---- envs ----------------------'
 keys.each do |key|
   if ENV[key].empty?
     raise "key: #{key} is not specified.abort."
   end
-  pp "[key: #{key}]"
+  puts "[key: #{key}]"
   pp ENV[key]
 end
 puts '--------------------------------'
 
-GITHUB_BASE_URL = ENV['GITHUB_BASE_URL']
+GITHUB_BASE_URL = "#{ENV['GITHUB_BASE_URL']}/#{ENV['REPOSITORY']}"
 TOKEN = ENV['GITHUB_TOKEN']
 SLACK_WEBHOOK_URL = ENV['SLACK_WEBHOOK']
 IGNORE_LABELS = ENV['IGNORE_LABELS'].split(',')
