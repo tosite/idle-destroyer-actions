18d060073ea06338d449f4ebdc393d0deab3c558
tosite <teshima.naoto@moneyforward.co.jp>
2023-11-29 12:54:24 +0900

    wip

--- actions/notify.rb
+++ actions/notify.rb
@@ -2,7 +2,7 @@ require '/actions/initializer'
 
 client = GitHubApiClient.new(base_url: GITHUB_BASE_URL, token: TOKEN, ignore_labels: IGNORE_LABELS)
 targets = client.fetch_old_issues_and_pulls(limit: LIMIT)
-issues = targets.select { !_1[:is_pr] }.map { "<#{_1[:title]}|#{_1[:url]}>"}
-pulls = targets.select { _1[:is_pr] }.map { "<#{_1[:title]}|#{_1[:url]}>"}
+issues = targets.select { !_1[:is_pr] }.map { "<#{_1[:url]}|#{_1[:title]}>"}
+pulls = targets.select { _1[:is_pr] }.map { "<#{_1[:url]}|#{_1[:title]}>"}
 
 SlackClient.new(webhook_url: SLACK_WEBHOOK_URL).send_slack_message(NOTIFY_TEMPLATE, issues, pulls)
