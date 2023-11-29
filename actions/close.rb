786e9887f1928f243edaed7adba5e69106919964
tosite <teshima.naoto@moneyforward.co.jp>
2023-11-29 13:01:18 +0900

    wip

--- actions/close.rb
+++ actions/close.rb
@@ -2,8 +2,9 @@ require '/actions/initializer'
 
 client = GitHubApiClient.new(base_url: GITHUB_BASE_URL, token: TOKEN, ignore_labels: IGNORE_LABELS)
 targets = client.fetch_old_issues_and_pulls(limit: LIMIT)
-issues = targets.select { !_1[:is_pr] }.map { "- [#{_1[:title]}](#{_1[:url]})"}
-pulls = targets.select { _1[:is_pr] }.map { "- [#{_1[:title]}](#{_1[:url]})"}
+issues = targets.select { !_1[:is_pr] }.map { "<#{_1[:url]}|#{_1[:title]}> - #{_1[:dates_not_updated]} days ago"}
+pulls = targets.select { _1[:is_pr] }.map { "<#{_1[:url]}|#{_1[:title]}> - #{_1[:dates_not_updated]} days ago"}
 
-targets.each { client.close(_1) }
+# targets.each { client.close(_1) }
+client.close(targets[0])
 SlackClient.new(webhook_url: SLACK_WEBHOOK_URL).send_slack_message(CLOSED_TEMPLATE, issues, pulls)
